cf4587aa7a3ef0085e403009b886c8408539abc3
bssam <bssammel@gmail.com>
2023-11-30 19:59:13 -0500

    state modification is set up for add to cart, now it will need to be added to an event handler before testing

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,5 +1,27 @@
+const ADD_TO_CART = 'cart/ADD_TO_CART';
+
+export const addToCart = (produceId) => {//this is the action creator
+    return {
+        type: ADD_TO_CART,
+        itemId//this is the payload with a key of the item id and a value of the item id that was passedin as a parameter
+        
+    }
+}
+
 export default function cartReducer (state = {}, action) {
     switch(action.type){
+        case ADD_TO_CART:{//new case for this new action
+            const {itemId} = action;
+            const newState = {//new state obj
+                ...state, //state defaults to a blank obj but if it has a value it will be passed in here from the parameter
+                itemId: {
+                    id: itemId, 
+                    count
+                }
+            };
+            return newState;
+            // action.
+        }
         default:
             return state;
     }
